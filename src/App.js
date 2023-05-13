deeb30b4a3ae175a8c2c2cda252219f0e43a3241
vishal958 <vishalmaurya958@gmail.com>
2023-05-13 14:09:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import FirstComponent from "./containers/FirstComponent";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <FirstComponent />
+    </div>
+  );
+}
